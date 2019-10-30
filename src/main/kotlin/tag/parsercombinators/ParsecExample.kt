39c8d1be2eb5d885de2bd53ea60909186b4ccfd5
ellib <elli.bleeker@gmail.com>
2019-10-30 17:39:55 +0100

    experimented with parser by adding an exclamation mark to the not parser; result was not the same as expected - INTERESTING

--- src/main/kotlin/tag/parsercombinators/ParsecExample.kt
+++ src/main/kotlin/tag/parsercombinators/ParsecExample.kt
@@ -2,11 +2,18 @@ package tag.parsercombinators
 
 import lambdada.parsec.io.Reader
 import lambdada.parsec.parser.*
+import java.net.URL
 
 fun main() {
-    val foo: Parser<Char, List<Char>> = not(char(',')).rep
-    val input = Reader.string("hello, parsec!")
+    // de type van foo is een functie "Parser" en Parser zelf krijgt ook types mee:
+    // Char en List (dat laatste heet "generics", m.a.w. je typeert een type)
+    val foo: Parser<Char, List<List<Char>>> = (not(char(',')).rep).rep
+    val input = Reader.string("hello, world!")
+    // de waarde van foobar is de Response van de parser die aan foo is toegewezen;
+    // de parser pakt de waarde van input
     val foobar = foo(input)
+    // de parser functie geeft een Response
+    // de Response heeft de subtypes Accept of Reject (-> data classes)
     println(foobar)
 
     when (foobar) {
