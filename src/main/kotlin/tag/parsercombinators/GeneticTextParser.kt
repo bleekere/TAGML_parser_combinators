5acae88ae8d7f2b73c13497804b293c56fb8ea0a
ellib <elli.bleeker@gmail.com>
2019-11-07 15:47:59 +0100

    added a close tag recognizer

--- src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
+++ src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
@@ -5,14 +5,16 @@ import lambdada.parsec.parser.*
 
 fun openTagRecog(): Parser<Char, List<Char>> = char('[') thenRight contentTagRecog() thenLeft char('>')
 
+fun closeTagRecog(): Parser<Char, List<Char>> = char('<') thenRight contentTagRecog() thenLeft char(']')
+
 fun contentTagRecog(): Parser<Char, List<Char>> = (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
 
 // fun closeTagRecog(): Parser<Char, Char> = char('')
 
 fun main() {
     // val foo: Parser<Char, List<Char>> = openTagRecog() thenRight (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
-    val foo : Parser<Char, List<Char>> = openTagRecog()
-    val input = Reader.string("[root>")
+    val foo : Parser<Char, Pair<List<Char>,List<Char>>> = openTagRecog() then closeTagRecog()
+    val input = Reader.string("[root><root]")
     val foobar = foo(input)
     println(foobar)
 
