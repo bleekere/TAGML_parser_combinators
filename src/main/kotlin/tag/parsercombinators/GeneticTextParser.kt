5657005986e8cbc02a3e4e93fd12dcb99c213a96
ellib <elli.bleeker@gmail.com>
2019-11-07 16:46:42 +0100

    concatenated the recognisers to process a string of TAGML tags containing both open, close, and optional tags

--- src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
+++ src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
@@ -2,6 +2,7 @@ package tag.parsercombinators
 
 import lambdada.parsec.io.Reader
 import lambdada.parsec.parser.*
+import java.io.Serializable
 
 // after: one parser after another, returning a list instead of a pair.
 infix fun <I, A> Parser<I, A>.after(p: Parser<I, A>): Parser<I, List<A>> = this flatMap { a -> p map { listOf(a, it) } }
@@ -14,14 +15,14 @@ fun closeTagRecog(): Parser<Char, List<Char>> = char('<') thenRight contentTagRe
 
 fun contentTagRecog(): Parser<Char, List<Char>> = (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
 
-// fun closeTagRecog(): Parser<Char, Char> = char('')
+fun openOptionalTagRecog(): Parser<Char, List<Char>> = string("[?") thenRight contentTagRecog() thenLeft char('>')
+
+fun closeOptionalTagRecog(): Parser<Char, List<Char>> = string("<?") thenRight contentTagRecog() thenLeft char(']')
 
 fun main() {
-    // val foo: Parser<Char, List<Char>> = openTagRecog() thenRight (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
-    val foo : Parser<Char, Pair<List<Char>,List<Char>>> = openTagRecog() then closeTagRecog()
-    val input = Reader.string("[root><root]")
+    val foo : Parser<Char, List<List<Char>>> = openTagRecog() after openOptionalTagRecog() after2 closeOptionalTagRecog() after2 closeTagRecog()
+    val input = Reader.string("[root>[?del><?del]<root]")
     val foobar = foo(input)
     println(foobar)
-
 }
 
