aed12cc98a2199994a4a166a25fe589917888fb3
Ronald Haentjens Dekker <ronald.dekker@di.huc.knaw.nl>
2019-11-07 16:29:01 +0100

    Update GeneticTextParser.kt
    Added after function. Acts like then, expect it returns a list instead of a pair.

--- src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
+++ src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
@@ -3,6 +3,9 @@ package tag.parsercombinators
 import lambdada.parsec.io.Reader
 import lambdada.parsec.parser.*
 
+// after: one parser after another, returning a list instead of a pair.
+infix fun <I, A> Parser<I, A>.after(p: Parser<I, A>): Parser<I, List<A>> = this flatMap { a -> p map { listOf(a, it) } }
+
 fun openTagRecog(): Parser<Char, List<Char>> = char('[') thenRight contentTagRecog() thenLeft char('>')
 
 fun closeTagRecog(): Parser<Char, List<Char>> = char('<') thenRight contentTagRecog() thenLeft char(']')
