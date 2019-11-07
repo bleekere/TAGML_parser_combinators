02a6a012c782d831c2a34d930a025e848eee8cd4
ellib <elli.bleeker@gmail.com>
2019-11-07 15:43:28 +0100

    condensed the tiny TAGML open tag parser by making a function of it

--- src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
+++ src/main/kotlin/tag/parsercombinators/GeneticTextParser.kt
@@ -3,10 +3,18 @@ package tag.parsercombinators
 import lambdada.parsec.io.Reader
 import lambdada.parsec.parser.*
 
+fun openTagRecog(): Parser<Char, List<Char>> = char('[') thenRight contentTagRecog() thenLeft char('>')
+
+fun contentTagRecog(): Parser<Char, List<Char>> = (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
+
+// fun closeTagRecog(): Parser<Char, Char> = char('')
+
 fun main() {
-    val foo: Parser<Char, List<Char>> = char('[') thenRight (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep thenLeft char('>')
+    // val foo: Parser<Char, List<Char>> = openTagRecog() thenRight (charIn(CharRange('a', 'z')) or charIn(CharRange('A','Z'))).rep
+    val foo : Parser<Char, List<Char>> = openTagRecog()
     val input = Reader.string("[root>")
     val foobar = foo(input)
     println(foobar)
 
 }
+
